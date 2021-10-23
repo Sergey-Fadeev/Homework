d759e46f41f66f4cc017d334d84bde0adc36ff79
Sergey Fadeev <59198513+Sergey-Fadeev@users.noreply.github.com>
2021-10-24 00:35:02 +0400

    Initial Commit

---
+++ Homework/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Homework
+//
+//  Created by Sergey on 23.10.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Homework/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Sergey on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UIImageView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var plusButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    var alert = UIAlertController(title: "Внимание!", message: "I am an alert message you cannot dissmiss.", preferredStyle: .alert)
+    let ok = UIAlertAction(title: "OK", style: .default)
+    
+    var counter: Int = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        counterLabel.text = String(counter)
+        alert.addAction(ok)
+        
+        minusButton.backgroundColor = .systemGreen
+        plusButton.backgroundColor = .systemGreen
+        resetButton.backgroundColor = .systemRed
+    }
+    
+    
+    @IBAction func minusButton(_ sender: Any) {
+        if counter > -10 {
+            counter -= 1
+            counterLabel.text = String(counter)
+        }else{
+            alert.message = "Достигнуто минимальное значение!"
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func plusButton(_ sender: Any) {
+        if counter < 10 {
+            counter += 1
+            counterLabel.text = String(counter)
+        }else{
+            alert.message = "Достигнуто максимальное значение!"
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func resetButton(_ sender: Any) {
+        counter = 0
+        counterLabel.text = String(counter)
+    }
+}
